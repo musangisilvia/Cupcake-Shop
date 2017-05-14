5775511b84c44a2ebe146550de1c9a330e17ea64
musangisilvia <musangisilvia@gmail.com>
2017-05-14 22:29:50 +0300

    email input

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-        $(".nav1").click(function() {
-          $(".spcl").show();
-          $(".gallery").hide();
-          $(".newt").hide();
-        });
-        $(".nav2").click(function() {
-          $(".newt").show();
-          $(".spcl").hide();
-          $(".gallery").hide();
-        });
-        $("nav3").click(function() {
-          $(".gallery").show();
-          $(".newt").hide();
-          $(".spcl").hide();
-        });
-      });
+    $(".nav1").click(function() {
+        $(".spcl").show();
+        $(".gallery").hide();
+        $(".newt").hide();
+    });
+    $(".nav2").click(function() {
+        $(".newt").show();
+        $(".spcl").hide();
+        $(".gallery").hide();
+    });
+    $("nav3").click(function() {
+        $(".gallery").show();
+        $(".newt").hide();
+        $(".spcl").hide();
+    });
+});
